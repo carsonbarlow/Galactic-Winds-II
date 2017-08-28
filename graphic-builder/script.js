9de4e43255627a9fefbd553f76a72e6131825628
carsonbarlow <carsonbarlow@yahoo.com>
2017-08-28 09:21:35 -0600

    changed input for color for svg builder

--- graphic-builder/script.js
+++ graphic-builder/script.js
@@ -1,33 +1,36 @@
 
 window.onload = function(e){
 
-  document.getElementById('stroke_color_red_up').addEventListener('click', stroke_color_red_up_pressed);
-  document.getElementById('stroke_color_red_down').addEventListener('click', stroke_color_red_down_pressed);
-  document.getElementById('stroke_color_green_up').addEventListener('click', stroke_color_green_up_pressed);
-  document.getElementById('stroke_color_green_down').addEventListener('click', stroke_color_green_down_pressed);
-  document.getElementById('stroke_color_blue_up').addEventListener('click', stroke_color_blue_up_pressed);
-  document.getElementById('stroke_color_blue_down').addEventListener('click', stroke_color_blue_down_pressed);
-
-  document.getElementById('fill_color_red_up').addEventListener('click', fill_color_red_up_pressed);
-  document.getElementById('fill_color_red_down').addEventListener('click', fill_color_red_down_pressed);
-  document.getElementById('fill_color_green_up').addEventListener('click', fill_color_green_up_pressed);
-  document.getElementById('fill_color_green_down').addEventListener('click', fill_color_green_down_pressed);
-  document.getElementById('fill_color_blue_up').addEventListener('click', fill_color_blue_up_pressed);
-  document.getElementById('fill_color_blue_down').addEventListener('click', fill_color_blue_down_pressed);
+  document.getElementById('stroke_color_input').addEventListener('input', stroke_color_input_changed);
+  document.getElementById('fill_color').addEventListener('input', fill_color_changed);
 
   document.getElementById('elips').addEventListener('click', elips_pressed);
   document.getElementById('rectangle').addEventListener('click', rectangle_pressed);
   document.getElementById('triangle').addEventListener('click', triangle_pressed);
+  document.getElementById('text').addEventListener('click', text_pressed);
+
+  document.getElementById('new_group').addEventListener('click', new_group_pressed);
+  document.getElementById('add_svg').addEventListener('click', add_svg_pressed);
+  document.getElementById('remove_svg').addEventListener('click', remove_svg_pressed);
+
+  document.getElementById('group_element_x').addEventListener('input', group_element_x_changed);
+  document.getElementById('group_element_y').addEventListener('input', group_element_y_changed);
+  document.getElementById('group_element_scale').addEventListener('input', group_element_scale_changed);
+  document.getElementById('group_element_rotation').addEventListener('input', group_element_rotation_changed);
 
 
-  input_stroke_width = document.getElementById('stroke_width_input');
-  input_stroke_width.addEventListener('input', stroke_width_input_changed);
-  input_height = document.getElementById('height_input');
-  input_height.addEventListener('input', height_input_changed);
-  input_width = document.getElementById('width_input');
-  input_width.addEventListener('input', width_input_changed);
-  input_top = document.getElementById('top_input');
-  input_top.addEventListener('input', top_input_changed);
+
+  document.getElementById('stroke_width_input').addEventListener('input', stroke_width_input_changed);
+
+  input_text = document.getElementById('text_input');
+  input_text.addEventListener('input', text_input_changed);
+  input_text_size = document.getElementById('text_size_input');
+  input_text_size.addEventListener('input', text_size_input_changed);
+
+
+  document.getElementById('height_input').addEventListener('input', height_input_changed);
+  document.getElementById('width_input').addEventListener('input', width_input_changed);
+  document.getElementById('top_input').addEventListener('input', top_input_changed);
 
   document.getElementById('save_svg').addEventListener('click', save_svg_pressed);
   document.getElementById('update_svg').addEventListener('click', update_svg_pressed);
@@ -35,142 +38,64 @@ window.onload = function(e){
   svg_list = document.getElementById('svg_list');
 };
 
-var input_stroke_width,
+var input_text,
+  input_text_size,
+  input_stroke_width,
   input_height,
   input_width,
   input_top,
   svg_list,
   svg_list_index;
 
-
-function stroke_color_red_up_pressed(e){
-  console.log('stroke_color_red_up pressed');
-  if (svg_stroke_R < 15){svg_stroke_R++;}
+function stroke_color_input_changed(e){
+  svg_stroke_color = e.target.value.substring(0,3);
   build_svg();
 };
 
-function stroke_color_red_down_pressed(e){
-  console.log('stroke_color_red_down pressed');
-  if (svg_stroke_R > 0){svg_stroke_R--;}
-  build_svg();
-};
-
-function stroke_color_green_up_pressed(e){
-  console.log('stroke_color_green_up pressed');
-  if (svg_stroke_G < 15){svg_stroke_G++;}
-  build_svg();
-};
-
-function stroke_color_green_down_pressed(e){
-  console.log('stroke_color_green_down pressed');
-  if (svg_stroke_G > 0){svg_stroke_G--;}
-  build_svg();
-};
-
-function stroke_color_blue_up_pressed(e){
-  console.log('stroke_color_blue_up pressed');
-  if (svg_stroke_B < 15){svg_stroke_B++;}
-  build_svg();
-};
-
-function stroke_color_blue_down_pressed(e){
-  console.log('stroke_color_blue_down pressed');
-  if (svg_stroke_B > 0){svg_stroke_B--;}
-  build_svg();
-};
-
-function fill_color_red_up_pressed(e){
-  console.log('fill_color_red_up pressed');
-  if (svg_fill_R < 15){svg_fill_R++;}
-  build_svg();
-};
-
-function fill_color_red_down_pressed(e){
-  console.log('fill_color_red_down pressed');
-  if (svg_fill_R > 0){svg_fill_R--;}
-  build_svg();
-};
-
-function fill_color_green_up_pressed(e){
-  console.log('fill_color_green_up pressed');
-  if (svg_fill_G < 15){svg_fill_G++;}
-  build_svg();
-};
-
-function fill_color_green_down_pressed(e){
-  console.log('fill_color_green_down pressed');
-  if (svg_fill_G > 0){svg_fill_G--;}
-  build_svg();
-};
-
-function fill_color_blue_up_pressed(e){
-  console.log('fill_color_blue_up pressed');
-  if (svg_fill_B < 15){svg_fill_B++;}
-  build_svg();
-};
-
-function fill_color_blue_down_pressed(e){
-  console.log('fill_color_blue_down pressed');
-  if (svg_fill_B > 0){svg_fill_B--;}
-  build_svg();
-};
-
-function elips_pressed(e){
-  console.log('elips pressed');
-  current_shape = 'e'
-  build_svg();
-};
-
-function rectangle_pressed(e){
-  console.log('rectangle pressed');
-  current_shape = 'r';
-  build_svg();
-};
-
-function triangle_pressed(e){
-  console.log('triangle pressed');
-  current_shape = 't';
-  build_svg();
-};
-
-function stroke_width_input_changed(e){
-  console.log('stroke_width_input changed');
-  stroke_width = parseFloat(stroke_width_input.value);
-  build_svg();
-};
-
-function height_input_changed(e){
-  console.log('height_input changed');
-  svg_height = parseInt(input_height.value);
-  build_svg();
-};
-
-function width_input_changed(e){
-  console.log('width_input changed');
-  svg_width = parseInt(input_width.value);
-  build_svg();
-};
-
-function top_input_changed(e){
-  console.log('top_input changed');
-  svg_top = parseInt(input_top.value);
+function fill_color_changed(e){
+  svg_fill_color = e.target.value.substring(0,3);
   build_svg();
 };
 
 function build_svg_specs(){
   var a = [
     current_shape,
-    num_to_hex[svg_fill_R],
-    num_to_hex[svg_fill_G],
-    num_to_hex[svg_fill_B],
+    svg_fill_color,
     svg_height,
     svg_width,
     svg_top
     ];
+  if (current_shape === 'x'){
+    a = [
+    current_shape,
+    svg_fill_color,
+    font_family,
+    font_size
+    ];
+  }
   if (stroke_width){
-    a.push('s'+num_to_hex[svg_stroke_R]+num_to_hex[svg_stroke_G]+num_to_hex[svg_stroke_B]+stroke_width);
+    a.push('s'+svg_stroke_color+stroke_width);
   }
   return a;
+};
+
+function unpack_svg_specs(a){
+  current_shape = a[0];
+  svg_fill_color = a[1];
+  if (current_shape !== 'x'){
+    svg_height = a[2];
+    svg_width = a[3];
+    svg_top = a[5];
+  }else{
+    font_family = a[2];
+    font_size = a[3];
+  }
+  for (var i = 4; i < a.length; i++){
+    if (a[i][0] == 's'){
+      svg_stroke_color = a[i].substring(1,4);
+      stroke_width = a[i].substring(4);
+    }
+  }
 };
 
 function save_svg_pressed(e){
@@ -216,6 +141,8 @@ function svg_div_clicked(e){
   for (i = 0; i < svg_list_children.length; i++){
     if (svg_list_children[i] == this){
       svg_list_index = i;
+      unpack_svg_specs(svg_array[i]);
+      build_svg();
     }
   }
 }
@@ -226,7 +153,8 @@ function svg_div_clicked(e){
 // DONE: update svg
 // DONE: delete saved svgs
 // DONE: make stroke optional
-// TODO: text and attributes
+// DONE: text and attributes
+// DONE: unpack svg from array
 // TODO: svg groups
 // TODO: add svgs to groups
 // TODO: remove svgs to groups
@@ -243,13 +171,8 @@ var svg_array_index = 0;
 var current_svg = '';
 var svg_display = document.getElementById('current_graphic');
 var stroke_width = 0;
-var svg_stroke_R = 0;
-var svg_stroke_G = 0;
-var svg_stroke_B = 0;
-
-var svg_fill_R = 0;
-var svg_fill_G = 0;
-var svg_fill_B = 0;
+var svg_stroke_color = '000';
+var svg_fill_color = '000';
 
 var svg_height = 10;
 var svg_width = 10;
@@ -257,21 +180,19 @@ var svg_top = 50;
 
 var current_shape = 'e';
 
-var num_to_hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-
 function build_ellipse(){
-  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><ellipse fill="#'+num_to_hex[svg_fill_R]+num_to_hex[svg_fill_G]+num_to_hex[svg_fill_B]+'" cx="50" cy="50" rx="'+svg_height+'" ry="'+svg_width+'" '+svg_stroke()+'/></svg>';
+  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><ellipse fill="#'+svg_fill_color+'" cx="50" cy="50" rx="'+svg_height+'" ry="'+svg_width+'" '+svg_stroke()+'/></svg>';
 };
 
 function svg_stroke(){
   if (stroke_width > 0){
-    return 'stroke="#'+num_to_hex[svg_stroke_R]+num_to_hex[svg_stroke_G]+num_to_hex[svg_stroke_B]+'" stroke-width="'+stroke_width+'"';
+    return 'stroke="#'+svg_stroke_color+'" stroke-width="'+stroke_width+'"';
   }
   return '';
 };
 
 function build_rectangle(){
-  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><rect fill="#'+num_to_hex[svg_fill_R]+num_to_hex[svg_fill_G]+num_to_hex[svg_fill_B]+'" x="'+((100-svg_width)/2)+'" y="'+((100-svg_height)/2)+'" width="'+svg_width+'" height="'+svg_height+'" '+svg_stroke()+' /></svg>';
+  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><rect fill="#'+svg_fill_color+'" x="'+((100-svg_width)/2)+'" y="'+((100-svg_height)/2)+'" width="'+svg_width+'" height="'+svg_height+'" '+svg_stroke()+' /></svg>';
 };
 
 function build_triangle(){
@@ -285,7 +206,15 @@ function build_triangle(){
   var x3 = svg_top;
   var y3 = (100 - svg_height)/2;
 
-  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><polygon fill="#'+num_to_hex[svg_fill_R]+num_to_hex[svg_fill_G]+num_to_hex[svg_fill_B]+'" points="'+x1+','+y1+','+x2+','+y2+','+x3+','+y3+'" '+svg_stroke()+' /></svg>';
+  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><polygon fill="#'+svg_fill_color+'" points="'+x1+','+y1+','+x2+','+y2+','+x3+','+y3+'" '+svg_stroke()+' /></svg>';
+};
+
+var svg_text = 'hello world';
+var font_family_enum = ['Arial','Verdana'];
+var font_family = 0;
+var font_size = 12;
+function build_text(){
+  current_svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.0" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 50 50" xml:space="preserve"><text y="50" fill="#'+svg_fill_color+'" font-family="'+font_family_enum[font_family]+'" font-size="'+font_size+'"'+svg_stroke()+' >'+svg_text+'</text></svg>';
 };
 
 function build_svg(){
@@ -293,8 +222,10 @@ function build_svg(){
     build_ellipse();
   }else if(current_shape === 'r'){
     build_rectangle();
-  }else{
+  }else if(current_shape === 't'){
     build_triangle();
+  }else{
+    build_text();
   }
 
   display_svg(current_svg);
@@ -302,4 +233,153 @@ function build_svg(){
 
 function display_svg(svg){
   svg_display.innerHTML = svg;
+};
+
+// TODO: get text in and out of here.
+var text_array = [];
+
+
+// ============================ Group Dynamics =====================================
+
+
+var applied_svg_list = [];
+var applied_svg_list_index = 0;
+
+var svg_index;
+var svg_x = 50;
+var svg_y = 50;
+var svg_scale = 1;
+var svg_rotation = 0;
+
+function build_applied_svg(){
+  var a = [
+    svg_index,
+    svg_x,
+    svg_y,
+    svg_scale,
+    svg_rotation
+  ];
+  return a;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============================ Event Functions =====================================
+
+function elips_pressed(e){
+  current_shape = 'e';
+  build_svg();
+};
+
+function rectangle_pressed(e){
+  current_shape = 'r';
+  build_svg();
+};
+
+function triangle_pressed(e){
+  current_shape = 't';
+  build_svg();
+};
+
+function text_pressed(e){
+  current_shape = 'x';
+  build_svg();
+};
+
+function new_group_pressed(e){
+  console.log('new_group pressed');
+};
+
+function add_svg_pressed(e){
+  console.log('add_svg pressed');
+};
+
+function remove_svg_pressed(e){
+  console.log('remove_svg pressed');
+};
+var svg_index;
+var svg_x = 50;
+var svg_y = 50;
+var svg_scale = 1;
+var svg_rotation = 0;
+
+
+// inputs
+function stroke_width_input_changed(e){
+  stroke_width = parseFloat(e.target.value);
+  build_svg();
+};
+
+function height_input_changed(e){
+  svg_height = parseInt(e.target.value);
+  build_svg();
+};
+
+function text_input_changed(e){
+  svg_text = input_text.value;
+  build_svg();
+};
+
+function text_size_input_changed(e){
+  font_size = input_text_size.value;
+  build_svg();
+};
+
+function width_input_changed(e){
+  svg_width = parseInt(e.target.value);
+  build_svg();
+};
+
+function top_input_changed(e){
+  svg_top = parseInt(e.target.value);
+  build_svg();
+};
+
+function group_element_x_changed(e){
+  svg_x = parseInt(e.target.value);
+  build_svg();
+};
+
+function group_element_y_changed(e){
+  svg_y = parseInt(e.target.value);
+  build_svg();
+};
+
+function group_element_scale_changed(e){
+  svg_scale = parseFloat(e.target.value);
+  build_svg();
+};
+
+function group_element_rotation_changed(e){
+  svg_rotation = parseInt(e.target.value);
+  build_svg();
 };
